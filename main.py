ed3b71161d343afdda671cd80b315bfefb652ee3
SombraCibernetica <zyrenzcampo@gmail.com>
2024-01-13 13:53:32 -0500

    Mi primer mensaje

---
+++ main.py
@@ -0,0 +1,12 @@
+def suma(numero1: int ,numero2: str):
+
+suma= numero1 + numero2
+
+if( suma  % 2 == 0):
+    print("el numero es :",suma )
+    print("es par")
+else:
+    print("el numero es :",suma )
+    print("es impar")
+
+suma(4,4)
